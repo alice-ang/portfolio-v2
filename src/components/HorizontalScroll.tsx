bc4f84084fd6a838a109fc490cd8b7f80b49431d
Alice A <alice.anglesjo@hotmail.com>
2024-05-30 13:34:12 +0200

    fix: next image

--- src/components/HorizontalScroll.tsx
+++ src/components/HorizontalScroll.tsx
@@ -2,6 +2,8 @@
 import { PreviewProject } from "@/lib/types";
 import { motion, useTransform, useScroll, useSpring } from "framer-motion";
 import { useRef } from "react";
+import Image from "next/image";
+import Link from "next/link";
 
 export const HorizontalScrollCarousel = ({
   projects,
@@ -27,21 +29,41 @@ export const HorizontalScrollCarousel = ({
           <h2 className="text-palette-yellow underline pl-8">Projects</h2>
           <motion.div style={{ x }} className="flex gap-16">
             {projects.map((project) => (
-              <div
+              <Link
+                href={`/projects/${project.id}`}
+                passHref
                 className="aspect-video min-h-[20vh] md:h-[40vh] flex justify-center items-center relative group "
-                style={{
-                  backgroundImage: `url(${project.images[0].url})`,
-                  backgroundSize: "cover",
-                  backgroundPosition: "center",
-                }}
                 key={project.id}
               >
+                <Image
+                  src={project.images[0].url}
+                  alt={project.title}
+                  fill
+                  className="aspect-video"
+                  placeholder="blur"
+                  blurDataURL={project.images[0].url}
+                />
                 <div className="absolute inset-0 w-full h-full bg-black bg-opacity-80 group-hover:bg-opacity-20 animation-transition "></div>
 
-                <h3 className="absolute group-hover:opacity-0 animation-transition ">
+                <h3 className="font-feta absolute group-hover:opacity-0 animation-transition ">
                   {project.title}
                 </h3>
-              </div>
+              </Link>
+              // <div
+              //   className="aspect-video min-h-[20vh] md:h-[40vh] flex justify-center items-center relative group "
+              //   style={{
+              //     backgroundImage: `url(${project.images[0].url})`,
+              //     backgroundSize: "cover",
+              //     backgroundPosition: "center",
+              //   }}
+              //   key={project.id}
+              // >
+              //   <div className="absolute inset-0 w-full h-full bg-black bg-opacity-80 group-hover:bg-opacity-20 animation-transition "></div>
+
+              //   <h3 className="absolute group-hover:opacity-0 animation-transition ">
+              //     {project.title}
+              //   </h3>
+              // </div>
             ))}
           </motion.div>
         </div>
