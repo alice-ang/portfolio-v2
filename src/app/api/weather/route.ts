bc66f17991a7de71d448c404e64ff475e2337f9a
Alice Anglesjö <aanglesjo@gmail.com>
2025-10-29 12:34:11 +0100

    new location

--- src/app/api/weather/route.ts
+++ src/app/api/weather/route.ts
@@ -5,9 +5,8 @@ export async function GET(req: NextRequest) {
   try {
     const apiKey = process.env.OPENWEATHERMAP_API_KEY;
 
-    const lat = "58.39027820";
-    const lon = "13.84612080";
-    const part = "minutely,hourly,alerts";
+    const lat = "58.588455";
+    const lon = "16.188313";
 
     const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&units=metric&exclude=minutely,hourly,alert, dailys&appid=${apiKey}`;
 
